3a0dbdfa3b962e9cc7e451f13a3c30aa0033fdef
Andriy Lupa <lupa_andriy@list.ru>
2019-07-24 13:17:29 +0300

    Updates

---
+++ precache-manifest.35afaaf0164761a7d2e501c101bd2195.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "575f7d6ee2cd1970d6d6e2a3f6946924",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "ad1c5f85c16be7b7029a",
+    "url": "/app/static/css/2.676dc204.chunk.css"
+  },
+  {
+    "revision": "eeb2d44333c12021d0dd",
+    "url": "/app/static/css/main.dadc716d.chunk.css"
+  },
+  {
+    "revision": "ad1c5f85c16be7b7029a",
+    "url": "/app/static/js/2.542c9a67.chunk.js"
+  },
+  {
+    "revision": "eeb2d44333c12021d0dd",
+    "url": "/app/static/js/main.aab3b045.chunk.js"
+  },
+  {
+    "revision": "127917d16c2343d0c8fa",
+    "url": "/app/static/js/runtime~main.d465ad9e.js"
+  },
+  {
+    "revision": "2afeea96bfeba9a86f27d764407bb52a",
+    "url": "/app/static/media/blue_white_gorizontal.2afeea96.svg"
+  }
+]);
